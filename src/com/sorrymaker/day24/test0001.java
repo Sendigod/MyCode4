e366ce4b5a2501a44e2ca720a3059f2ad65ee48a
sen <sorrymaker@qq.com>
2021-06-30 20:03:03 +0800

    003

---
+++ src/com/sorrymaker/day24/test0001.java
@@ -0,0 +1,13 @@
+package com.sorrymaker.day24;
+
+/**
+ * @Author nextGame
+ * @Date 2021/6/30 20:02
+ * @Version 1.0
+ */
+public class test0001 {
+
+    public static void main(String[] args) {
+        System.out.println("push");
+    };
+}
